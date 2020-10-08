aee00ffaef68877f8e6b64e6a11e9b110cd28cf6
Alexander Petrovski <APetrovski5069296@gmail.com>
2020-10-08 22:25:16 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -7,14 +7,18 @@ module.exports = function repeater(str, options) {
   options.separator = options.separator || '\+';
   options.additionSeparator = options.additionSeparator || '|';
   options.repeatTimes = options.repeatTimes || 1;
+  options.additionRepeatTimes = options.additionRepeatTimes || 1;
+  str = (typeof str === 'string') ? str : String(str);
+  options.addition = (typeof options.addition === 'string') ? options.addition : String(options.addition);
   for ( var i = 0; i < options.repeatTimes; i++) {
-    result.push(str);
+    if (str) result.push(str);
     for ( var j = 0; j < options.additionRepeatTimes; j++ ){
-      result.push(options.addition);
-      if ( j - 1 < options.additionRepeatTimes && options.additionRepeatTimes !== 1)  result.push(options.additionSeparator);
+      if (options.addition &&  options.addition !== 'undefined') result.push(options.addition);
+      if ( j +1 < options.additionRepeatTimes && options.additionRepeatTimes !== 1)  result.push(options.additionSeparator);
     }
-    if (i -1 < options.repeatTimes) result.push(options.separator)
+    if (i+1 < options.repeatTimes) result.push(options.separator)
   }
-  return result.join('') + ' -->' + options.repeatTimes + ' ' + options.separator + ' ' + options.additionRepeatTimes + ' ' +String(options.additionSeparator);
+  return result.join(''); 
+  //return result.join('') + ' -->' + options.repeatTimes + ' ' + options.separator + ' '+ options.addition+ ' ' + options.additionRepeatTimes + ' ' +String(options.additionSeparator);
 };
   
