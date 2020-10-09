7a16b42064d6d02d6142d7911ebc054c78d02788
Alexander Petrovski <APetrovski5069296@gmail.com>
2020-10-09 13:45:46 +0300

    feat: add solution vigenere-cipher, have problems

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,37 @@
 const CustomError = require("../extensions/custom-error");
-
+const CODE_A = 65;
+const LENGTH_ALPHABET = 26;
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
+  constructor (type){
+    this.reverseType = type === false;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error()
+  }
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    let flag = 0;
+  key = key.toUpperCase().split('').map( item => item.charCodeAt(0) - CODE_A );
+  message = message.toUpperCase().split('').map( (item,i) => (/[A-Z]/.test(item)) ? String.fromCharCode((item.charCodeAt(0) - CODE_A + key[(i - flag) % key.length]) % LENGTH_ALPHABET + CODE_A) : (flag++, item));
+  return this.reverseType ? message.reverse().join('') : message.join('');
+
+
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error()
+  }
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    let flag = 0;
+    key = key.toUpperCase().split('').map( item => item.charCodeAt(0) - CODE_A );
+    message = message.toUpperCase().split('').map( (item,i) => (/[A-Z]/.test(item)) ? String.fromCharCode((item.charCodeAt(0) - CODE_A +LENGTH_ALPHABET - key[(i - flag) % key.length]) % LENGTH_ALPHABET + CODE_A) : (flag++, item));
+    return this.reverseType ? message.reverse().join('') : message.join('');
+
+
+
+
   }
 }
 
